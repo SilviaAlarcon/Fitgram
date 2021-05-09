c55a9da04610197780ae96816f0a5715286e17fd
Silvia Alarcón <silviaalarcon@MacBook-Air-de-Silvia.local>
2021-05-09 16:58:05 -0500

    added validation at login

--- mindgram/urls.py
+++ mindgram/urls.py
@@ -9,12 +9,16 @@ from django.contrib import admin
 
 from mindgram import views as local_views
 from posts import views as posts_views
+from users import views as users_views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('holis', local_views.hello_world),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi),
+    path('holis', local_views.hello_world, name='hello world'),
+    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
 
-    path('posts/', posts_views.list_posts),
+    path('posts/', posts_views.list_posts, name='feed'),
+
+    path('users/login/', users_views.login_view, name='login')
+
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- users/views.py
+++ users/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+#Users views
 
-# Create your views here.
+#Django
+from django.contrib.auth import authenticate, login
+from django.shortcuts import render, redirect
+
+
+def login_view(request):
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request, username=username, password=password)
+        if user is not None:
+            login(request, user)
+            return redirect('feed')
+        else:
+            return render(request, 'users/login.html', {'error': 'Invalidad username or password'})
+
+    return render(request, 'users/login.html')
