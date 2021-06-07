429e027b5806f6ff85b8a4573471e16bfc64b4c5
Silvia Alarcón <silviaalarcon@MacBook-Air-de-Silvia.local>
2021-06-07 01:45:41 -0500

    form validation

--- users/forms.py
+++ users/forms.py
@@ -1,7 +1,51 @@
 #User form 
 
 #Django 
-from django import forms 
+from django import forms
+from django.forms.fields import CharField
+from django.forms.widgets import EmailInput 
+
+#Models
+from django.contrib.auth.models import User
+from users.models import Profile
+
+
+class SignupForm(forms.Form):
+    username = forms.CharField(min_length=5, max_length=50)
+    password = forms.CharField(max_length=100, widget=forms.PasswordInput())
+    password_confirmation = forms.CharField(max_length=100, widget=forms.PasswordInput())
+    first_name = forms.CharField(min_length=2, max_length=50)
+    last_name = forms.CharField(min_length=2, max_length=50)
+    email = forms.CharField(min_length=6, max_length=60, widget=forms.EmailInput())
+
+    #Username must be unique
+    def clean_username(self):
+        username = self.cleaned_data['username']
+        username_taken = User.objects.filter(username=username).exists()
+        if username_taken:
+            raise forms.ValidationError('Username is already in use.')
+        return username
+
+    #Verify password confirmation match
+    def clean(self):
+        data = super().clean()
+
+        password = data['password']
+        password_confirmation = data['password_confirmation']
+
+        if password != password_confirmation:
+            raise forms.ValidationError("Passwords do not match")
+
+        return data 
+
+    #Create user and profile
+    def save(self):
+        data = self.cleaned_data
+        data.pop('password_confirmation')
+
+        user = User.objects.create_user(**data)
+        profile = Profile(user = user)
+        profile.save()
 
 
 class ProfileForm(forms.Form):
--- users/views.py
+++ users/views.py
@@ -5,15 +5,8 @@ from django.contrib.auth import authenticate, login, logout
 from django.contrib.auth.decorators import login_required
 from django.shortcuts import render, redirect
 
-#Exception
-from django.db.utils import IntegrityError
-
-#Models
-from django.contrib.auth.models import User
-from users.models import Profile
-
 #Forms
-from users.forms import ProfileForm
+from users.forms import ProfileForm, SignupForm
 
 
 def login_view(request):
@@ -31,36 +24,20 @@ def login_view(request):
 
 
 def signup_view(request):
-    #The POST method is validated
     if request.method == 'POST':
-        username = request.POST['username']
-        password = request.POST['password']
-        password_confirmation = request.POST['password_confirmation']
+        form = SignupForm(request.POST)
 
-        #Password validation 
-        if password != password_confirmation:
-            return render(request, 'users/signup.html', {'error': 'Password confirmation does not match'})
-
-        #Exception in case the username already exists 
-        try:
-            #User creation
-            user = User.objects.create_user(username=username, password=password)
-        except IntegrityError:
-            return render(request, 'users/signup.html', {'error': 'Username is already in user'})
-
-        #The remaining fields are added to the created user
-        user.first_name = request.POST['first_name']
-        user.last_name = request.POST['last_name']
-        user.email = request.POST['email'] 
-        user.save()
-
-        #User profile is created 
-        profile = Profile(user=user)     
-        profile.save()  
-
-        return redirect('login')
-
-    return render(request, 'users/signup.html')
+        if form.is_valid():
+            form.save()
+            return redirect('login')
+    else:
+        form = SignupForm()
+    
+    return render(
+        request=request,
+        template_name= 'users/signup.html',
+        context={'form': form}
+    )
 
 
 @login_required
