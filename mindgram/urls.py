300bea808c761ec0bd16257bdc10de5203fcf2e1
Silvia Alarcón <silviaalarcon@MacBook-Air-de-Silvia.local>
2021-06-11 02:42:23 -0500

    Use of Class-based views

--- mindgram/urls.py
+++ mindgram/urls.py
@@ -1,28 +1,17 @@
 #Mindgram URLs module
 
 #Django
-from django.urls import path
+from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib import admin
 
 
-from mindgram import views as local_views
-from posts import views as posts_views
-from users import views as users_views
-
 urlpatterns = [
     path('admin/', admin.site.urls),
 
-    path('holis', local_views.hello_world, name='hello world'),
-    path('hi/<str:name>/<int:age>/', local_views.say_hi, name='hi'),
+    path('', include(('posts.urls', 'posts'), namespace='posts')),
 
-    path('', posts_views.list_posts, name='feed'),
-    path('posts/new/', posts_views.create_post, name='create_post'),
-
-    path('users/login/', users_views.login_view, name='login'),
-    path('users/logout/', users_views.logout_view, name='logout'),
-    path('users/signup/', users_views.signup_view, name='signup'),
-    path('users/me/profile/', users_views.update_profile, name='update_profile')
+    path('users/', include(('users.urls', 'users'), namespace='users')),
 
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
